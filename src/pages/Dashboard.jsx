7fec0332890a2873c776324af376f6a1bf9dd11c
florien2208 <mahorompakanyiflorien@gmail.com>
2023-10-11 14:23:33 +0200

    small updates

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -89,35 +89,35 @@ function Dashboard() {
                       ></input>
                     </div>
                   </div>
-                </Link>
-                <br />
-                <div className="divisionformat2">
-                  <div>
-                    <h6>#XM9141</h6>
+
+                  <br />
+                  <div className="divisionformat2">
+                    <div>
+                      <h6>#XM9141</h6>
+                    </div>
+                    <div>
+                      <h6>25 august 2022</h6>
+                    </div>
+                    <div>
+                      <h6>mathias</h6>
+                    </div>
+                    <div>
+                      <h6>$123</h6>
+                    </div>
+                    <div>
+                      <input
+                        className="black"
+                        type="submit"
+                        value="Paid"
+                        style={{
+                          color: "#33d69f",
+                          height: "30px",
+                          width: "75px",
+                        }}
+                      ></input>
+                    </div>
                   </div>
-                  <div>
-                    <h6>25 august 2022</h6>
-                  </div>
-                  <div>
-                    <h6>mathias</h6>
-                  </div>
-                  <div>
-                    <h6>$123</h6>
-                  </div>
-                  <div>
-                    <input
-                      className="black"
-                      type="submit"
-                      value="Paid"
-                      style={{
-                        color: "#33d69f",
-                        height: "30px",
-                        width: "75px",
-                      }}
-                    ></input>
-                  </div>
-                </div>
-                {/* <div className="divisionformat2">
+                  {/* <div className="divisionformat2">
                   <div>
                     <h6>#XM9141</h6>
                   </div>
@@ -142,115 +142,116 @@ function Dashboard() {
                     ></input>
                   </div>
                 </div> */}
-                <br />
-                <div className="divisionformat2">
-                  <div>
-                    <h6>#YM9141</h6>
+                  <br />
+                  <div className="divisionformat2">
+                    <div>
+                      <h6>#YM9141</h6>
+                    </div>
+                    <div>
+                      <h6>30 july 2022</h6>
+                    </div>
+                    <div>
+                      <h6>Moses</h6>
+                    </div>
+                    <div>
+                      <h6>$300</h6>
+                    </div>
+                    <div>
+                      <input
+                        className="black1"
+                        type="submit"
+                        value="Pending"
+                        style={{
+                          color: "#ff8f00",
+                          height: "30px",
+                          width: "75px",
+                        }}
+                      ></input>
+                    </div>
                   </div>
-                  <div>
-                    <h6>30 july 2022</h6>
+                  <br />
+                  <div className="divisionformat2">
+                    <div>
+                      <h6>#YM9141</h6>
+                    </div>
+                    <div>
+                      <h6>30 july 2022</h6>
+                    </div>
+                    <div>
+                      <h6>Mufrobiba</h6>
+                    </div>
+                    <div>
+                      <h6>$500</h6>
+                    </div>
+                    <div>
+                      <input
+                        className="black1"
+                        type="submit"
+                        value="Pending"
+                        style={{
+                          color: "#ff8f00",
+                          height: "30px",
+                          width: "75px",
+                        }}
+                      ></input>
+                    </div>
                   </div>
-                  <div>
-                    <h6>Moses</h6>
+                  <br />
+                  <div className="divisionformat2">
+                    <div>
+                      <h6>#YM9141</h6>
+                    </div>
+                    <div>
+                      <h6>30 july 2022</h6>
+                    </div>
+                    <div>
+                      <h6>Mahendra</h6>
+                    </div>
+                    <div>
+                      <h6>$500</h6>
+                    </div>
+                    <div>
+                      <input
+                        className="black"
+                        type="submit"
+                        value="Draft"
+                        style={{
+                          color: "white",
+                          height: "30px",
+                          width: "75px",
+                        }}
+                      ></input>
+                    </div>
                   </div>
-                  <div>
-                    <h6>$300</h6>
-                  </div>
-                  <div>
-                    <input
-                      className="black1"
-                      type="submit"
-                      value="Pending"
-                      style={{
-                        color: "#ff8f00",
-                        height: "30px",
-                        width: "75px",
-                      }}
-                    ></input>
-                  </div>
-                </div>
-                <br />
-                <div className="divisionformat2">
-                  <div>
-                    <h6>#YM9141</h6>
-                  </div>
-                  <div>
-                    <h6>30 july 2022</h6>
-                  </div>
-                  <div>
-                    <h6>Mufrobiba</h6>
-                  </div>
-                  <div>
-                    <h6>$500</h6>
-                  </div>
-                  <div>
-                    <input
-                      className="black1"
-                      type="submit"
-                      value="Pending"
-                      style={{
-                        color: "#ff8f00",
-                        height: "30px",
-                        width: "75px",
-                      }}
-                    ></input>
-                  </div>
-                </div>
-                <br />
-                <div className="divisionformat2">
-                  <div>
-                    <h6>#YM9141</h6>
-                  </div>
-                  <div>
-                    <h6>30 july 2022</h6>
-                  </div>
-                  <div>
-                    <h6>Mahendra</h6>
-                  </div>
-                  <div>
-                    <h6>$500</h6>
-                  </div>
-                  <div>
-                    <input
-                      className="black"
-                      type="submit"
-                      value="Draft"
-                      style={{
-                        color: "white",
-                        height: "30px",
-                        width: "75px",
-                      }}
-                    ></input>
-                  </div>
-                </div>
 
-                <br />
-                <div className="divisionformat2">
-                  <div>
-                    <h6>#YM9141</h6>
+                  <br />
+                  <div className="divisionformat2">
+                    <div>
+                      <h6>#YM9141</h6>
+                    </div>
+                    <div>
+                      <h6>30 july 2022</h6>
+                    </div>
+                    <div>
+                      <h6>Moses</h6>
+                    </div>
+                    <div>
+                      <h6>$300</h6>
+                    </div>
+                    <div>
+                      <input
+                        className="black1"
+                        type="submit"
+                        value="Pending"
+                        style={{
+                          color: "#ff8f00",
+                          height: "30px",
+                          width: "75px",
+                        }}
+                      ></input>
+                    </div>
                   </div>
-                  <div>
-                    <h6>30 july 2022</h6>
-                  </div>
-                  <div>
-                    <h6>Moses</h6>
-                  </div>
-                  <div>
-                    <h6>$300</h6>
-                  </div>
-                  <div>
-                    <input
-                      className="black1"
-                      type="submit"
-                      value="Pending"
-                      style={{
-                        color: "#ff8f00",
-                        height: "30px",
-                        width: "75px",
-                      }}
-                    ></input>
-                  </div>
-                </div>
+                </Link>
               </div>
             </div>
           </>
